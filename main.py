e4aeaab3be356b86073c41e245142adf0a15bd82
TKyleB <kyle.beasley22@gmail.com>
2024-12-24 14:27:45 -0600

    Added player class to drawable & updatable containers

--- main.py
+++ main.py
@@ -9,14 +9,21 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt = 0
     
+    # Groups
+    drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    Player.containers = (drawable, updatable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        for object in updatable:
+            object.update(dt)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
     print(f"Screen width: {SCREEN_WIDTH}")
